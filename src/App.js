4176daff5987c40d265fe2f9ae5e33d3ea6d8d6f
aung-khantkyaw <aungkhantkyaw.19-20@ucsmub.edu.mm>
2024-04-07 09:26:42 +0630

    First day - Component, Props, useState, useRef

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import "./App.css";
 
-function App() {
+function Item() {
+  return <li>Item</li>;
+}
+
+function ItemWithProps(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <li>
+      {props.name} - {props.id}
+    </li>
   );
 }
 
-export default App;
+export default function App() {
+  const [items, setItems] = useState([
+    { name: "Item", id: "1" },
+    { name: "Item", id: "2" },
+  ]);
+
+  const addItem = () => {
+    const id = items.length + 1;
+    const name = "Item";
+    setItems([...items, { name, id }]);
+  };
+
+  const nameRef = useRef();
+  const idRef = useRef();
+
+  const addNewItem = () => {
+    let name = nameRef.current.value;
+    let id = idRef.current.value;
+
+    setItems([...items, { name, id }]);
+  };
+
+  return (
+    <div>
+      <h3>React Learning</h3>
+
+      <h5>Component</h5>
+      <Item />
+
+      <h5>Component with props</h5>
+      <ItemWithProps name="Item" id="1" />
+
+      <h5>useState</h5>
+      {items.map((item) => (
+        <ItemWithProps name={item.name} id={item.id} />
+      ))}
+      <button onClick={addItem}>Add Item</button>
+
+      <h5>useRef</h5>
+      <input type="text" ref={nameRef} />
+      <input type="text" ref={idRef} />
+      <button onClick={addNewItem}>Add New Item</button>
+    </div>
+  );
+}
